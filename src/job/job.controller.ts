c94e350c6d538617313a082e8d26c5e8f30d338b
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2019-05-11 23:58:11 +0300

    initial commit

---
+++ src/job/job.controller.ts
@@ -0,0 +1,7 @@
+import Controller from "../interfaces/controller.interface";
+class JobController implements Controller {
+  constructor() {
+    this.initializeRoutes();
+  }
+  private initializeRoutes() {}
+}
